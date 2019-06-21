f61665fae2f59004cf81350d83c37ae8f0d2e4fb
niubiwu <wu15770563284@163.com>
2019-06-21 17:34:19 +0800

    分离组件，login功能完善

--- showprodutes/src/components/index/index.vue
+++ showprodutes/src/components/index/index.vue
@@ -1,115 +1,35 @@
 <template>
   <el-container>
+    <!-- 导入header组件 -->
     <el-header>
-      <el-row>
-        <el-col :span="4">
-          <div class="grid-content bg-purple">
-            <h1>
-              <img src="../../assets/logo.png">
-            </h1>
-          </div>
-        </el-col>
-        <el-col :span="16">
-          <div class="grid-content bg-purple-light">
-            <h2>黑马电商后台管理系统</h2>
-          </div>
-        </el-col>
-        <el-col :span="4">
-          <div class="grid-content bg-purple">
-            <a href="/login">退出</a>
-          </div>
-        </el-col>
-      </el-row>
+      <Header></Header>
     </el-header>
+
     <el-container>
+      <!-- 导入sidebar组件 -->
       <el-aside width="200px">
-        <el-menu
-          default-active="2"
-          :span="24"
-          class="el-menu-vertical-demo"
-          @open="handleOpen"
-          @close="handleClose"
-        >
-          <el-submenu index="1">
-            <template slot="title">
-              <i class="el-icon-location"></i>
-              <span>用户管理</span>
-            </template>
-            <el-menu-item index="1-1">
-              <i class="el-icon-menu"></i>
-              <span slot="title">用户列表</span>
-            </el-menu-item>
-          </el-submenu>
-          <el-submenu index="2">
-            <template slot="title">
-              <i class="el-icon-location"></i>
-              <span>权限管理</span>
-            </template>
-            <el-menu-item index="2-1">
-              <i class="el-icon-menu"></i>
-              <span slot="title">角色列表</span>
-            </el-menu-item>
-            <el-menu-item index="2-2">
-              <i class="el-icon-menu"></i>
-              <span slot="title">权限列表</span>
-            </el-menu-item>
-          </el-submenu>
-          <el-submenu index="3">
-            <template slot="title">
-              <i class="el-icon-location"></i>
-              <span>商品管理</span>
-            </template>
-            <el-menu-item index="3-1">
-              <i class="el-icon-menu"></i>
-              <span slot="title">商品列表</span>
-            </el-menu-item>
-            <el-menu-item index="3-2">
-              <i class="el-icon-menu"></i>
-              <span slot="title">分类参数</span>
-            </el-menu-item>
-            <el-menu-item index="3-3">
-              <i class="el-icon-menu"></i>
-              <span slot="title">商品分类</span>
-            </el-menu-item>
-          </el-submenu>
-          <el-submenu index="4">
-            <template slot="title">
-              <i class="el-icon-location"></i>
-              <span>订单管理</span>
-            </template>
-            <el-menu-item index="4-1">
-              <i class="el-icon-menu"></i>
-              <span slot="title">订单列表</span>
-            </el-menu-item>
-          </el-submenu>
-          <el-submenu index="5">
-            <template slot="title">
-              <i class="el-icon-location"></i>
-              <span>数据统计</span>
-            </template>
-            <el-menu-item index="5-1">
-              <i class="el-icon-menu"></i>
-              <span slot="title">数据报表</span>
-            </el-menu-item>
-          </el-submenu>
-        </el-menu>
+        <sideBar></sideBar>
       </el-aside>
+
       <el-main>Main</el-main>
     </el-container>
   </el-container>
 </template>
 
 <script>
+// 引入组件header
+import Header from "../header/header";
+// 引入组件header
+import sideBar from "../sidebar/sidebar";
 export default {
+  components: {
+    Header: Header,
+    sideBar: sideBar
+  },
   methods: {
-    handleOpen (key, keyPath) {
-      console.log(key, keyPath)
-    },
-    handleClose (key, keyPath) {
-      console.log(key, keyPath)
-    }
+    // 登录退出功能
   }
-}
+};
 </script>
 
 <style>
@@ -134,13 +54,9 @@ export default {
   background-color: #fff;
   color: #333;
   height: 100%;
-  text-align: center;
   line-height: 200px;
 }
 
-.el-aside ul li .el-submenu__title span {
-  padding-right: 50px;
-}
 .el-main {
   background-color: #e9eef3;
   color: #333;

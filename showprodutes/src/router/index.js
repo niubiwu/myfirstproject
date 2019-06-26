1e868ff43d1baaf2ee1a094031e2bd19e087998c
niubiwu <wu15770563284@163.com>
2019-06-26 16:48:46 +0800

    登录验证全局使用功能成功

--- showprodutes/src/router/index.js
+++ showprodutes/src/router/index.js
@@ -9,7 +9,7 @@ import commoditylist from "@/components/commodityList/commodityList.vue";
 import addgoods from "@/components/commodityList/addGoods.vue";
 Vue.use(Router)
 
-export default new Router({
+let router = new Router({
   routes: [{
     path: '/login',
     name: 'login',
@@ -41,3 +41,23 @@ export default new Router({
     }]
   }]
 })
+// 全局路由守卫匹配使用登录令牌
+router.beforeEach((to, from, next) => {
+  //判断是否是跳转到登录页面，不是到登录页面需要验证token
+  //to是个跳转的路由对像
+  if (to.name !== 'login') {
+    // 验证是否登录登录，没有登录就回到登陆界面
+    var token = window.localStorage.getItem('token')
+
+    if (!token) {
+      router.push({
+        name: 'login'
+      })
+    } else {
+      next()
+    }
+  } else {
+    next()
+  }
+})
+export default router
